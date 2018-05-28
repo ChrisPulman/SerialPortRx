1a06b4009da85f495da5c8b5f05cc4387cfc03e0
Chris Pulman <chris.pulman@yahoo.com>
2018-05-28 23:03:58 +0100

    Updated to RxProperty v4.2.2

--- SerialPortRx/Properties/AssemblyInfo.cs
+++ SerialPortRx/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("cda3585b-d3d6-4be4-9a6b-582ab5de0b8f")]
-[assembly: AssemblyVersion("0.0.0.5")]
-[assembly: AssemblyFileVersion("0.0.0.5")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
