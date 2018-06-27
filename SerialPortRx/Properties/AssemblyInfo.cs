a0e46fde7701d3a1448dceae41c4b53bd05bc823
Chris Pulman <chris.pulman@yahoo.com>
2018-06-27 13:04:24 +0100

    Updated Version number

--- SerialPortRx/Properties/AssemblyInfo.cs
+++ SerialPortRx/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("cda3585b-d3d6-4be4-9a6b-582ab5de0b8f")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
